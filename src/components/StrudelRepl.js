69330b055ecdc1f993ca2596a60e649c8e44edd3
Gholam <105897233+Gholam-Warasi@users.noreply.github.com>
2025-10-23 20:45:01 +1030

    Update StrudelRepl.js
    To seprate the instance from app.js to its own method

--- src/components/StrudelRepl.js
+++ src/components/StrudelRepl.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+import React from 'react';
+
+export default function StrudelRepl() {
+    return (
+        <div className="repl-container mt-3">
+            <label className="form-label">Strudel REPL (Processed Output):</label>
+            {/* The StrudelMirror instance will attach itself here */}
+            <div id="strudel-repl-editor" style={{ maxHeight: '50vh', overflowY: 'auto' }} />
+            <div id="output" />
+            {/* Note: The <canvas> for pianoroll is omitted for brevity */}
+        </div>
+    );
+}
