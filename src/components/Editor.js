b3d39202ef0e96a058a6e376d0af5d776c3133dc
Gholam <105897233+Gholam-Warasi@users.noreply.github.com>
2025-11-03 19:20:52 +1030

    implement basic Editor UI with textarea and label

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+import React from 'react';
+
+function Editor({ value, onChange }) {
+    return (
+        <div className="editor-container">
+            <label htmlFor="proc-editor" className="form-label">Text to preprocess:</label>
+            <textarea
+                className="form-control"
+                rows="15"
+                id="proc-editor"
+                value={value}
+                onChange={onChange}
+                style={{ fontFamily: "monospace" }}
+            >
+            </textarea>
+        </div>
+    );
+}
+
+export default Editor; 
