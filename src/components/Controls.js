6befb66af9697c4c1a82c6f2911528ac6df8dbb6
Gholam <105897233+Gholam-Warasi@users.noreply.github.com>
2025-11-05 15:36:26 +1030

    Added Drum Pattern dropdown

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -82,7 +82,21 @@ function Controls({ controls, setControls, onPlay, onStop, onProcAndPlay }) {
                 </select>
             </div>
 
-
+            {/* Drum Pattern */}
+            <div className="mb-3">
+                <label htmlFor="drum_pattern" className="form-label">Drum Pattern</label>
+                <select
+                    className="form-select"
+                    id="drum_pattern"
+                    name="drum_pattern"
+                    value={controls.drum_pattern}
+                    onChange={handleChange}
+                >
+                    <option value="0">Pattern 1 (Quiet)</option>
+                    <option value="1">Pattern 2 (Buildup)</option>
+                    <option value="2">Pattern 3 (Full)</option>
+                </select>
+            </div>
 
         </div>
     );
