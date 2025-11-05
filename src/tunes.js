487a65b059b2f2bef55498b1923da690c26cc3c1
Gholam <105897233+Gholam-Warasi@users.noreply.github.com>
2025-11-05 21:46:39 +1030

    new beats

--- src/tunes.js
+++ src/tunes.js
@@ -1,93 +1,49 @@
-export const stranger_tune = `setcps(<tempo_control>/60/4)
-samples('github:algorave-dave/samples')
-samples('https://raw.githubusercontent.com/tidalcycles/Dirt-Samples/master/strudel.json')
-samples('https://raw.githubusercontent.com/Mittans/tidal-drum-machines/main/machines/tidal-drum-machines.json')
-
-const gain_patterns = [
-  "2",
-  "{0.75 2.5}*4",
-    "{0.75 2.5!9 0.75 2.5!5 0.75 2.5 0.75 2.5!7 0.75 2.5!3 <2.5 0.75> 2.5}%16",
-]
-
-const drum_structure = [
- "~",
- "x*4",
- "{x ~!9 x ~!5 x ~ x ~!7 x ~!3 < ~ x > ~}%16",
-]
-
-const basslines = [
-  "[[eb1, eb2]!16 [f2, f1]!16 [g2, g1]!16 [f2, f1]!8 [bb2, bb1]!8]/8",
-  "[[eb1, eb2]!16 [bb2, bb1]!16 [g2, g1]!16 [f2, f1]!4 [bb1, bb2]!4 [eb1, eb2]!4 [f1, f2]!4]/8"
-]
-
-const arpeggiator1 = [
-  "{d4 bb3 eb3 d3 bb2 eb2}%16",
-  "{c4 bb3 f3 c3 bb2 f2}%16",
-  "{d4 bb3 g3 d3 bb2 g2}%16",
-  "{c4 bb3 f3 c3 bb2 f2}%16",
-]
-
-const arpeggiator2 = [
-  "{d4 bb3 eb3 d3 bb2 eb2}%16",
-  "{c4 bb3 f3 c3 bb2 f2}%16",
-  "{d4 bb3 g3 d3 bb2 g2}%16",
-  "{d5 bb4 g4 d4 bb3 g3 d4 bb3 eb3 d3 bb2 eb2}%16",
-]
+export const jazzlike = `setcps(<tempo_control>/60/4)
+// Based on the style of Felix Roos (CC BY-NC-SA 4.0)
+await samples('github:tidalcycles/Dirt-Samples/master/')
 
 const pattern = <drum_pattern_control>
 
-<p1_Radio>bassline:
-note(pick(basslines, <bassline_control>))
-.sound("supersaw")
-.postgain(2)
-.room(<reverb_control>)
-.lpf(700)
-.gain(<master_volume_control>)
-
-<p1_Radio>main_arp: 
-note(pick(arpeggiator1, <arpeggiator_control>))
-.sound("supersaw")
-.lpf(300)
-.adsr("0:0:.5:.1")
-.room(<reverb_control>)
-.lpenv(3.3)
-.postgain(pick(gain_patterns, pattern))
-.gain(<master_volume_control>)
-
-drums:
+/* DRUMS */
+<mute_drums>drums:
 stack(
-  s("tech:5")
-  .postgain(6)
-  .pcurve(2)
-  .pdec(1)
-  .struct(pick(drum_structure, pattern)),
-  s("sh").struct("[x!3 ~!2 x!10 ~]")
-  .postgain(0.5).lpf(7000)
-  .bank("RolandTR808")
-  .speed(0.8).jux(rev).room(sine.range(0.1,0.4)).gain(0.6),
-  s("{~ ~ rim ~ cp ~ rim cp ~!2 rim ~ cp ~ < rim ~ >!2}%8 *2")
-  .bank("[KorgDDM110, OberheimDmx]").speed(1.2)
-  .postgain(.25),
-).gain(<master_volume_control>)
+  s("bd").struct(pick(["x ~ x ~","x*2 ~ x ~ x*2 ~ ~","{x ~!3 x x ~ x!2 x ~}%8"], pattern)).postgain(2).pdec(1).pcurve(2).gain(<gain_drums> * <master_volume_control>),
+  s("sn").struct("{~ s ~ s}%4").postgain(1).lpf(8000).gain(<gain_drums> * <master_volume_control>),
+  s("hh").struct("x*16").postgain(0.6).jux(rev).room(<reverb_control>).gain(<gain_drums> * <master_volume_control>)
+).gain(1)
+<mute_drums>
 
-drums2: 
+// BASS
+<mute_bass>bassline:
+note(pick(["<eb1 eb2 eb3 eb2>/8","<f2 f3 f2 f1>/8"], <bassline_control>))
+.sound("sawtooth")
+.cutoff(400)
+.resonance(0.7)
+.legato(0.5)
+.gain(<gain_bass> * <master_volume_control>)
+.room(<reverb_control>)
+<mute_bass>
+
+// LEAD / MELODY
+<mute_lead>lead:
+note(pick(["~ e5 ~ ~ ~ c5 ~ d5","~ e6 ~ ~ c6 ~ d6 ~"], <arpeggiator_control>))
+.sound("triangle")
+.decay(0.25)
+.sustain(0.12)
+.gain(<gain_lead> * <master_volume_control>)
+.delay(<delay_send_control>)
+.delaytime(0.375)
+.delayfeedback(0.45)
+.room(<reverb_control>)
+<mute_lead>
+
+// Optional second drum layer (toggled by show_drums2)
+drums2:
 stack(
-  s("[~ hh]*4").bank("RolandTR808").room(0.3).speed(0.75).gain(1.2),
-  s("hh").struct("x*16").bank("RolandTR808")
-  .gain(0.6)
-  .jux(rev)
-  .room(sine.range(0.1,0.4))
-  .postgain(0.5),
-  
-  s("[psr:[2|5|6|7|8|9|12|24|25]*16]?0.1")
-  .gain(0.1)
-  .postgain(pick(gain_patterns, pattern))
-  .hpf(1000)
-  .speed(0.5)
-  .rarely(jux(rev)),
-).gain(<master_volume_control>)
+  "[bd ~ ~ ~] [~ ~ ~ ~] [~ ~ bd ~] [~ ~ bd ~] ",
+  "[hh ~ hh ~] [hh ~ hh ~] [hh ~ hh ~] [hh ~ hh ~] ",
+  "[~ ~ ~ ~] [sd ~ ~ ~] [~ ~ ~ ~] [sd ~ ~ ~] ",
+).s().slow(2)
 
-//Remixed and reproduced from Algorave Dave's code found here: https://www.youtube.com/watch?v=ZCcpWzhekEY
-// all(x => x.gain(mouseX.range(0,1)))
-all(x => x.log())
-// @version 1.2`;
+
+`;
