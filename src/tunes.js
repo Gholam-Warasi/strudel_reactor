deff4ee39ee4b1f85e6a3262b8d7455d33f73852
Gholam <105897233+Gholam-Warasi@users.noreply.github.com>
2025-11-06 07:37:36 +1030

    Update tunes.js

--- src/tunes.js
+++ src/tunes.js
@@ -1,13 +1,12 @@
 export const jazzlike = `setcps(<tempo_control>/60/4)
 // Based on the style of Felix Roos (CC BY-NC-SA 4.0)
 await samples('github:tidalcycles/Dirt-Samples/master/')
-
 const pattern = <drum_pattern_control>
 
 /* DRUMS */
 <mute_drums>drums:
 stack(
-  s("bd").struct(pick(["x ~ x ~","x*2 ~ x ~ x*2 ~ ~","{x ~!3 x x ~ x!2 x ~}%8"], pattern)).postgain(2).pdec(1).pcurve(2).gain(<gain_drums> * <master_volume_control>),
+  s("bd").struct(pick(["x ~ x ~","x*2 ~ x ~ x*2 ~ ~","{x ~!3 x x ~ x!2 x ~}%8"], pattern)).postgain(2).pdec(1).pcurve(2).gain(<gain_drums> * <master_volume_control>).log("drums:<gain_drums>"),
   s("sn").struct("{~ s ~ s}%4").postgain(1).lpf(8000).gain(<gain_drums> * <master_volume_control>),
   s("hh").struct("x*16").postgain(0.6).jux(rev).room(<reverb_control>).gain(<gain_drums> * <master_volume_control>)
 ).gain(1)
@@ -22,7 +21,20 @@ note(pick(["<eb1 eb2 eb3 eb2>/8","<f2 f3 f2 f1>/8"], <bassline_control>))
 .legato(0.5)
 .gain(<gain_bass> * <master_volume_control>)
 .room(<reverb_control>)
+.log("bass:<gain_bass>")
 <mute_bass>
+
+// CHORDS
+<mute_chords>chords:
+note("<[c3,eb3,g3] [f3,ab3,c4] [g3,bb3,d4] [eb3,g3,bb3]>")
+.sound("sawtooth")
+.cutoff(800)
+.resonance(0.3)
+.legato(0.8)
+.gain(<gain_chords> * <master_volume_control>)
+.room(<reverb_control>)
+.log("chords:<gain_chords>")
+<mute_chords>
 
 // LEAD / MELODY
 <mute_lead>lead:
@@ -35,6 +47,7 @@ note(pick(["~ e5 ~ ~ ~ c5 ~ d5","~ e6 ~ ~ c6 ~ d6 ~"], <arpeggiator_control>))
 .delaytime(0.375)
 .delayfeedback(0.45)
 .room(<reverb_control>)
+.log("lead:<gain_lead>")
 <mute_lead>
 
 // Optional second drum layer (toggled by show_drums2)
